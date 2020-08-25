8a82a9a512c81110416b9799e5b2285be4b434ad
Matthew Milici <milici.m915@gmail.com>
2020-08-24 19:07:30 -0700

    second page added

---
+++ dist/scripts/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $("#thumbsDown").on("click", goodDay);
+    $("#thumbsUp").on("click", goodDay);
+});
+
+function goodDay() {
+    const userAnswer = $(this).attr("id");
+    $("#thumbsUp").hide();
+    $("#thumbsDown").hide();
+    $("#reasons").addClass("show");
+    $("#reasons").removeClass("hidden");
+
+    if (userAnswer === "thumbsUp") {
+        $("#hey-there").text("nice!");
+        $("#message").text("Why did you have a good day?");
+    } else {
+        $("#hey-there").text("ahh bummer!");
+        $("#message").text("Why did you have a bad day?");
+    }
+}
