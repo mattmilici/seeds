28f9e671afb3608f5b04f51a2f40385569ae5097
Matthew Milici <milici.m915@gmail.com>
2020-08-26 21:45:07 -0700

    added object constructor

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -1,50 +1,48 @@
 $(document).ready(function() {
-    $("#currentDate").text(moment().format("MM/DD/YYYY"));
+    function dailyData(day, date, status, reason) {
+        this.day = day;
+        this.date = date;
+        this.status = status;
+        this.reason = reason;
+    }
+
+    var currentDayTic = new dailyData();
+
+    const currentDate = moment().format("MM/DD/YYYY");
+    $("#currentDate").text(currentDate);
     $("#thumbsDown").on("click", goodDay);
     $("#thumbsUp").on("click", goodDay);
-});
+    $(".reasonWhy").on("click", reasonWhy);
 
-function goodDay() {
-    const userAnswer = $(this).attr("id");
-    $("#thumbsUp").hide();
-    $("#thumbsDown").hide();
-    $("#reasons").addClass("show");
-    $("#reasons").removeClass("hidden");
+    //-----------------------------------goodDay start-----------------------------------
+    //this is what determines the message and options the user sees after determining if it was a good or bad day
+    function goodDay() {
+        const userAnswer = $(this).attr("id");
+        $("#thumbsUp").hide();
+        $("#thumbsDown").hide();
+        $("#reasons").addClass("show");
+        $("#reasons").removeClass("hidden");
 
-    if (userAnswer === "thumbsUp") {
-        $("#hey-there").text("nice!");
-        $("#message").text("Why did you have a good day?");
-    } else {
-        $("#hey-there").text("ahh bummer!");
-        $("#message").text("Why did you have a bad day?");
+        if (userAnswer === "thumbsUp") {
+            $("#hey-there").text("nice!");
+            $("#message").text("Why did you have a good day?");
+        } else {
+            $("#hey-there").text("ahh bummer!");
+            $("#message").text("Why did you have a bad day?");
+        }
+        currentDayTic.status = userAnswer;
+        currentDayTic.date = currentDate;
+        console.log(currentDayTic);
     }
-}
 
-//google charts start
-google.charts.load("current", { packages: ["corechart"] });
-google.charts.setOnLoadCallback(drawChart);
+    //-----------------------------------goodDay end-----------------------------------
+    //----------------------------------- Reason Why start-----------------------------------
+    function reasonWhy() {
+        const userReason = $(this).attr("id");
+        console.log(userReason);
+        currentDayTic.reason = userReason;
+        console.log(currentDayTic);
+    }
 
-function drawChart() {
-    var data = google.visualization.arrayToDataTable([
-        ["Task", "Hours per Day"],
-        ["work", 11],
-        ["family", 2],
-        ["sleep", 2],
-        ["diet", 5],
-        ["not sure", 2],
-    ]);
-
-    var options = {
-        title: "",
-        pieHole: 0.4,
-        pieSliceText: "none",
-        backgroundColor: "transparent",
-    };
-
-    var chart = new google.visualization.PieChart(
-        document.getElementById("donutchart")
-    );
-    chart.draw(data, options);
-}
-
-//google charts end
+    //-----------------------------------Reason Why end-----------------------------------
+});
